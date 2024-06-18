22fe3e06c54e3cc5ae88256cdfca797d90e493e6
leroymusa <musaleroyohio@gmail.com>
2024-06-18 17:52:32 -0400

    refactor: README update

---
+++ __src__/scripts/install_libraries.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+arduino-cli lib install "Adafruit Sensor"
+arduino-cli lib install "Adafruit BMP280 Library"
+arduino-cli lib install "Adafruit BNO055"
+arduino-cli lib install "RadioHead"
+arduino-cli lib install "ArduinoJson"
+arduino-cli lib install "Eigen"

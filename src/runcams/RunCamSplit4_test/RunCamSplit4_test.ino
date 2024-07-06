254d1a772166fd7ac5bf257bc27ff06bb0961bc7
leroymusa <musaleroyohio@gmail.com>
2024-07-06 11:25:28 -0400

    add: added updated runcam logic

--- src/runcams/RunCamSplit4_test/RunCamSplit4_test.ino
+++ src/runcams/RunCamSplit4_test/RunCamSplit4_test.ino
@@ -1,43 +1,73 @@
-#include <Arduino.h>
+#define BUFF_SIZE 20
+#define HWSERIAL Serial1
 
-// CRC-8 calculation using polynomial 0xD5
-byte calculateCRC(byte *data, byte len) {
-    byte crc = 0;
-    for (byte i = 0; i < len; i++) {
-        crc ^= data[i];
-        for (byte j = 0; j < 8; j++) {
-            if (crc & 0x80)
-                crc = (crc << 1) ^ 0xD5;  // Polynomial for DVB-S2 CRC-8
-            else
-                crc <<= 1;
-        }
+uint8_t txBuf[BUFF_SIZE], crc;
+int recState = 0;
+
+void setup(void) 
+{
+    Serial.begin(9600);  
+    HWSERIAL.begin(115200);
+    //give the runcam time to send whatever it does out of reset
+    delay(3000);
+
+    //serial command to toggle recording
+    //more info on the serial packet structure --> https://support.runcam.com/hc/en-us/articles/360014537794-RunCam-Device-Protocol
+    txBuf[0] = 0xCC;
+    txBuf[1] = 0x01;
+    txBuf[2] = 0x01;  
+    txBuf[3] = calcCrc(txBuf, 3);  //compute the CRC
+    Serial.println(txBuf[3]);
+
+    // Start recording after 10 seconds
+    delay(10000);
+    startRecording();
+}
+
+void loop() 
+{
+    // Nothing to do in the loop for this example
+}
+
+void startRecording() 
+{
+    if(recState == 0) 
+    {
+        Serial.println("Starting Recording");
+        recState = 1;
+        HWSERIAL.write(txBuf, 4);
     }
+}
+
+void stopRecording() 
+{
+    if(recState == 1)
+    {
+        Serial.println("Stopping Recording");
+        recState = 0;
+        HWSERIAL.write(txBuf, 4);
+    }
+}
+
+uint8_t calcCrc(uint8_t *buf, uint8_t numBytes)
+{
+    uint8_t crc = 0;
+    for(uint8_t i = 0; i < numBytes; i++)
+        crc = crc8_calc(crc, *(buf + i), 0xd5);
+        
     return crc;
 }
 
-void setup() {
-  Serial1.begin(115200); // Set the baud rate to match the camera's requirement
-  Serial.begin(9600);    // Start serial communication with computer for debugging
-
-  pinMode(13, OUTPUT);   // Set pin 13 as output for the LED
-
-  // Command to start recording
-  byte startCommand[] = {0xCC, 0x03}; // Command byte for starting recording
-  byte startCRC = calculateCRC(startCommand, sizeof(startCommand));
-  Serial1.write(startCommand, sizeof(startCommand));
-  Serial1.write(startCRC);
-  digitalWrite(13, HIGH); // Turn on LED to indicate recording has started
-  Serial.println("Recording started.");
-
-  delay(10000); // Record for 10 seconds
-
-  // Command to stop recording
-  byte stopCommand[] = {0xCC, 0x04}; // Command byte for stopping recording
-  byte stopCRC = calculateCRC(stopCommand, sizeof(stopCommand));
-  Serial1.write(stopCommand, sizeof(stopCommand));
-  Serial1.write(stopCRC);
-  digitalWrite(13, LOW); // Turn off LED after recording stops
-  Serial.println("Recording stopped.");
+uint8_t crc8_calc(uint8_t crc, unsigned char a, uint8_t poly)
+{
+    crc ^= a;
+    for(int ii = 0; ii < 8; ++ii) 
+    {
+        if(crc & 0x80)
+            crc = (crc << 1) ^ poly;
+        else
+            crc = crc << 1;
+    }
+    
+    return crc;
 }
-
-void loop() {} // do nothing
